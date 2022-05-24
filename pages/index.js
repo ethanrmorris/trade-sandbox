0c080f45b305f65b4c0046905e69dff81cc05ed8
ethanrmorris <ethan.morris33@gmail.com>
2022-05-24 12:23:43 -0500

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,61 @@ export default function Home({ results }) {
             <a>
               <div key={trade.id} className={styles.card}>
                 <p>{trade.date}</p>
-                <h2>
-                  {trade.owner_1
-                    .charAt(0)
-                    .toUpperCase()
-                    .concat(trade.owner_1.slice(1))}
-                </h2>
-                <h2>
-                  {trade.owner_2
-                    .charAt(0)
-                    .toUpperCase()
-                    .concat(trade.owner_2.slice(1))}
-                </h2>
+                <div className={styles.tradeDiv}>
+                  <div>
+                    <h2>
+                      {trade.owner_1
+                        .charAt(0)
+                        .toUpperCase()
+                        .concat(trade.owner_1.slice(1))}
+                    </h2>
+                    {trade.players_1.map((player) => (
+                      <p>
+                        {player.name}
+                        <span>
+                          {' '}
+                          {player.position + ' -'} {player.team}
+                        </span>
+                      </p>
+                    ))}
+                  </div>
+                  <div>
+                    <h2>
+                      {trade.owner_2
+                        .charAt(0)
+                        .toUpperCase()
+                        .concat(trade.owner_2.slice(1))}
+                    </h2>
+                    {trade.players_2.map((player) => (
+                      <p>
+                        {player.name}
+                        <span>
+                          {' '}
+                          {player.position + ' -'} {player.team}
+                        </span>
+                      </p>
+                    ))}
+                  </div>
+                  {trade.owner_3 && (
+                    <div>
+                      <h2>
+                        {trade.owner_3
+                          .charAt(0)
+                          .toUpperCase()
+                          .concat(trade.owner_3.slice(1))}
+                      </h2>
+                      {trade.players_3.map((player) => (
+                        <p>
+                          {player.name}
+                          <span>
+                            {' '}
+                            {player.position + ' -'} {player.team}
+                          </span>
+                        </p>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </div>
             </a>
           </Link>
