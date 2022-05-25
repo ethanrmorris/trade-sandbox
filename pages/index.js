d30e71b4ecdd3dbf7a6f334cab458b22485a556f
ethanrmorris <ethan.morris33@gmail.com>
2022-05-25 16:57:46 -0500

    4 files modified, 1 added, 1 deleted

--- pages/index.js
+++ pages/index.js
@@ -7,92 +7,88 @@ export default function Home({ results }) {
       <h1>Chug Trades</h1>
       <div className={styles.grid}>
         {results.map((trade) => (
-          <Link href={`/trades/${trade.id}`} as={`/trades/${trade.id}`}>
-            <a>
-              <div key={trade.id} className={styles.card}>
-                <p>{trade.date}</p>
-                <div className={styles.tradeDiv}>
-                  <div>
-                    <h2>
-                      {trade.owner_1
-                        .charAt(0)
-                        .toUpperCase()
-                        .concat(trade.owner_1.slice(1))}
-                      {' Acquires'}
-                    </h2>
-                    {trade.players_1.map((player) => (
-                      <p className={styles.playerName}>
-                        {player.name}
-                        {player.player ? (
-                          <div
-                            className={styles.player}
-                          >{`(${player.player}, ${player.position} - ${player.team})`}</div>
-                        ) : (
-                          <span>
-                            {player.position
-                              ? `  ${player.position} - ${player.team}`
-                              : null}
-                          </span>
-                        )}
-                      </p>
-                    ))}
-                  </div>
-                  <div>
-                    <h2>
-                      {trade.owner_2
-                        .charAt(0)
-                        .toUpperCase()
-                        .concat(trade.owner_2.slice(1))}
-                      {' Acquires'}
-                    </h2>
-                    {trade.players_2.map((player) => (
-                      <p className={styles.playerName}>
-                        {player.name}
-                        {player.player ? (
-                          <div
-                            className={styles.player}
-                          >{`(${player.player}, ${player.position} - ${player.team})`}</div>
-                        ) : (
-                          <span>
-                            {player.position
-                              ? `  ${player.position} - ${player.team}`
-                              : null}
-                          </span>
-                        )}
-                      </p>
-                    ))}
-                  </div>
-                  {trade.owner_3 && (
-                    <div>
-                      <h2>
-                        {trade.owner_3
-                          .charAt(0)
-                          .toUpperCase()
-                          .concat(trade.owner_3.slice(1))}
-                        {' Acquires'}
-                      </h2>
-                      {trade.players_3.map((player) => (
-                        <p className={styles.playerName}>
-                          {player.name}
-                          {player.player ? (
-                            <div
-                              className={styles.player}
-                            >{`(${player.player}, ${player.position} - ${player.team})`}</div>
-                          ) : (
-                            <span>
-                              {player.position
-                                ? `  ${player.position} - ${player.team}`
-                                : null}
-                            </span>
-                          )}
-                        </p>
-                      ))}
-                    </div>
-                  )}
-                </div>
+          <div key={trade.id} className={styles.card}>
+            <p>{trade.date}</p>
+            <div className={trade.owner_3 ? styles.tradeDiv3 : styles.tradeDiv}>
+              <div>
+                <h2>
+                  {trade.owner_1
+                    .charAt(0)
+                    .toUpperCase()
+                    .concat(trade.owner_1.slice(1))}
+                  {' Acquires'}
+                </h2>
+                {trade.players_1.map((player) => (
+                  <p className={styles.playerName}>
+                    {player.name}
+                    {player.player ? (
+                      <div
+                        className={styles.player}
+                      >{`(${player.player}, ${player.position} - ${player.team})`}</div>
+                    ) : (
+                      <span>
+                        {player.position
+                          ? `  ${player.position} - ${player.team}`
+                          : null}
+                      </span>
+                    )}
+                  </p>
+                ))}
               </div>
-            </a>
-          </Link>
+              <div>
+                <h2>
+                  {trade.owner_2
+                    .charAt(0)
+                    .toUpperCase()
+                    .concat(trade.owner_2.slice(1))}
+                  {' Acquires'}
+                </h2>
+                {trade.players_2.map((player) => (
+                  <p className={styles.playerName}>
+                    {player.name}
+                    {player.player ? (
+                      <div
+                        className={styles.player}
+                      >{`(${player.player}, ${player.position} - ${player.team})`}</div>
+                    ) : (
+                      <span>
+                        {player.position
+                          ? `  ${player.position} - ${player.team}`
+                          : null}
+                      </span>
+                    )}
+                  </p>
+                ))}
+              </div>
+              {trade.owner_3 && (
+                <div>
+                  <h2>
+                    {trade.owner_3
+                      .charAt(0)
+                      .toUpperCase()
+                      .concat(trade.owner_3.slice(1))}
+                    {' Acquires'}
+                  </h2>
+                  {trade.players_3.map((player) => (
+                    <p className={styles.playerName}>
+                      {player.name}
+                      {player.player ? (
+                        <div
+                          className={styles.player}
+                        >{`(${player.player}, ${player.position} - ${player.team})`}</div>
+                      ) : (
+                        <span>
+                          {player.position
+                            ? `  ${player.position} - ${player.team}`
+                            : null}
+                        </span>
+                      )}
+                    </p>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
         ))}
       </div>
     </div>
