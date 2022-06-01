bb2664d80f99dc6cc9d4fa8b7cccaf97367ebabe
ethanrmorris <ethan.morris33@gmail.com>
2022-06-01 14:27:59 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Home({ results }) {
   return (
     <div className={styles.container}>
-      <h1>Chug Trades</h1>
+      <h1>Chug League Trades</h1>
       <div className={styles.gap}>
         <Link href="/">
           <a>Trades</a>
